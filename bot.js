cd589657e4a65309317b7e02a7ef51d2c79d4353
Arjunser <91903465+Arjunser@users.noreply.github.com>
2021-10-04 22:18:01 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 /* Copyright (C) 2020 Yusuf Usta.
-RECODDED BY AFNANPPLK
+RECODDED BY ARJUN SER
 Plk  
 */
 
@@ -133,7 +133,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
         });
 
         console.log(
-            chalk.green.bold('Badboy bot working perfectly 👻')
+            chalk.green.bold('Arjunser_ bot working perfectly 👻')
         );
     });
     
@@ -237,7 +237,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                             await command.function(whats, match);
                         } catch (error) {
                             if (config.LANG == 'TR' || config.LANG == 'AZ') {
-                                await conn.sendMessage(conn.user.jid, '-- HATA RAPORU [BADBOYBOT] --' + 
+                                await conn.sendMessage(conn.user.jid, '-- HATA RAPORU [Arjunser_BOT] --' + 
                                     '\n*WhatsAsena bir hata gerçekleşti!*'+
                                     '\n_Bu hata logunda numaranız veya karşı bir tarafın numarası olabilir. Lütfen buna dikkat edin!_' +
                                     '\n_Yardım için Telegram grubumuza yazabilirsiniz._' +
@@ -245,7 +245,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, 'BADBOYBOT_🐱_[error] ' +
+                                await conn.sendMessage(conn.user.jid, 'Arjunser_BOT_🐱_[error] ' +
                                     '\n\n*👻 ' + error + '*\n'
                                     , MessageType.text);
                             }
